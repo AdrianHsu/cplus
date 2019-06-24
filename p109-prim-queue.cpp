4c5486bd78682ca258f871cbee83b026286cbff8
AdrianHsu <adrianhsu1995@gmail.com>
2019-06-25 02:03:11 +0800

    [UPDATED] Tue Jun 25 02:03:11 CST 2019 :bulb:

--- p109-prim-queue.cpp
+++ p109-prim-queue.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdio>
 #include <cmath>
 #include <cstring>
@@ -11,45 +12,53 @@
 using namespace std;
 
 struct edge { int to, cost; };
-typedef pair<int, int> P; // first: distance, second: idx
+typedef pair<int, int> P; // first: mincost, second: idx
 
 //int V;
-vector<edge> G[MAX_V];
-int d[MAX_V];
-int pre[MAX_V];
+vector<edge> G[MAX_V]; // un-directed graph
+int mincost[MAX_V];
+int pre[MAX_V]; // prev
 
-void dijkstra(int s) {
+void prim(int s) {
   priority_queue<P, vector<P>, greater<P> > que;
-  fill(d, d + MAX_V, INF);
+  fill(mincost, mincost + MAX_V, INF);
   fill(pre, pre + MAX_V, -1);
-  d[s] = 0;
+  mincost[s] = 0;
   que.push(P(0, s));
+  
+  vector<bool> inMST(MAX_V, false);
+  int res = 0;
 
   while(!que.empty()){
     P p = que.top(); que.pop();
     int v = p.second;
-    if(d[v] < p.first) continue;
+    inMST[v] = true; 
+    if(mincost[v] < p.first) continue;
     for (int i = 0; i < G[v].size(); i ++) {
       edge e = G[v][i];
-      if(d[e.to] > d[v] + e.cost) {
-        d[e.to] = d[v] + e.cost;
-        que.push(P(d[e.to], e.to));
+      if(!inMST[e.to] && mincost[e.to] > e.cost) {
+        mincost[e.to] = e.cost;
+        que.push(P(mincost[e.to], e.to));
         pre[e.to] = v;
       }
     }
+    res += mincost[v];
   }
+  printf("Total Min Cost: %d\n", res);
 }
-void printDistance(){
+void printMinCost(){
+  printf("MINCOST...\n");
   for(int i = 0; i < MAX_V; i++)
-    printf("%d: %d\n", i, d[i]);
+    printf("%d: %d\n", i, mincost[i]);
 }
 void printPre(){
   printf("PREV VERTEX...\n");
   for(int i = 0; i < MAX_V; i++)
     printf("%d: %d\n", i, pre[i]);
 }
+
 int main() {
-  // ./run.sh p105-dijkstra-queue.cpp < in/dijkstra-sample.in  
+  // ./run.sh p109-prim-queue.cpp < in/prim-sample.in  
   int s;
   edge e;
   int eof = 1;
@@ -61,9 +70,9 @@ int main() {
     G[s].push_back(e);
   }
   printf("------\n");
-  dijkstra(1);
-  printDistance();
-  printf("------\n");
+  prim(1);
+  printMinCost();
+  printf("-----\n");
   printPre();
   return 0;
 }
