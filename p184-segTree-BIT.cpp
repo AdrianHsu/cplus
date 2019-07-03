9e95153120cf0545b077ec080b9847128cbe5f83
AdrianHsu <adrianhsu1995@gmail.com>
2019-07-03 23:43:53 +0800

    [UPDATED] Wed Jul  3 23:43:53 CST 2019 :bulb:

--- p184-segTree-BIT.cpp
+++ p184-segTree-BIT.cpp
@@ -7,16 +7,15 @@
 #define MAX_Q 100000
 
 using namespace std;
-
+ 
 typedef long long ll;
 
 int N, Q;
-ll A[MAX_N];
+ll A[MAX_N + 1];
 char T[MAX_Q];
 int L[MAX_Q], R[MAX_Q], X[MAX_Q];
 
-ll data[DAT_SIZE];
-ll datb[DAT_SIZE];
+ll data[DAT_SIZE], datb[DAT_SIZE];
 
 void add(int a, int b, int x, int k, int l, int r) {
   if(a <= l && r <= b) {
@@ -65,11 +64,54 @@ void solve() {
   }
 }
 
+//////////////////////////////////
+
+ll bit0[MAX_N + 1], bit1[MAX_N + 1];
+
+ll sumBIT(ll *b, int i) {
+  ll s = 0;
+  while(i > 0) {
+    s += b[i];
+    i -= i&-i;
+  }
+  return s;
+}
+
+void addBIT(ll *b, int i, int v) {
+  while(i <= N) {
+    b[i] += v;
+    i += i & -i;
+  }
+}
+
+void solveBIT() {
+  memset(bit0, 0, sizeof(bit0));
+  memset(bit1, 0, sizeof(bit1));
+
+  for(int i = 1; i <= N; i++) {
+    addBIT(bit0, i, A[i]);
+  }
+  for(int i = 0; i < Q; i++) {
+    if(T[i] == 'C') {
+      addBIT(bit0, L[i], -X[i] * (L[i] - 1));
+      addBIT(bit1, L[i], X[i]);
+      addBIT(bit0, R[i] + 1, X[i] * R[i]);
+      addBIT(bit1, R[i] + 1, -X[i]);
+    } else {
+      ll res = 0;
+      res += sumBIT(bit0, R[i]) + sumBIT(bit1, R[i]) * R[i];
+      res -= sumBIT(bit0, L[i] - 1) + sumBIT(bit1, L[i] - 1) * (L[i] - 1);
+      printf("%lld\n", res);
+    }
+  }
+}
+
 int main() {
 
   scanf("%d%d", &N, &Q);
   for(int i = 0; i < N; i ++) {
-    scanf("%lld", &A[i]);
+    //scanf("%lld", &A[i]); // if using SegTree
+    scanf("%lld", &A[i + 1]); // if using BIT
   }
   for(int i = 0; i < Q; i++) {
     cin >> T[i];
@@ -79,6 +121,6 @@ int main() {
       scanf("%d%d%d", &L[i], &R[i], &X[i]);
     }
   }
-  solve();
+  solveBIT();
   return 0;
 }
