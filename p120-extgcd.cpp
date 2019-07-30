57bbc4446f75ee359b3379b4b4c38347c2cd4d11
AdrianHsu <adrianhsu1995@gmail.com>
2019-07-30 17:04:13 +0800

    [UPDATED] Tue Jul 30 17:04:12 CST 2019 :bulb:

---
+++ p120-extgcd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int extgcd(int a, int b, int &x, int &y) {
+  
+  int d = a; 
+  if(b != 0) {
+    d = extgcd(b, a % b, x, y);
+    int x_prime = x;
+    x = y;
+    y = x_prime - (a / b) * y;
+  } else {
+    x = 1; y = 0;
+  }
+
+  return d;
+}
+
+int main() {
+  int x, y;
+  cout << extgcd(12, 42, x, y) << endl;
+  cout << x << "," << y << endl;
+  return 0;
+}
