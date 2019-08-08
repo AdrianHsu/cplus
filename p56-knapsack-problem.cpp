0060f63e72f7ef92d28f7ee9954ad7c6665f8e54
AdrianHsu <adrianhsu1995@gmail.com>
2019-08-08 22:35:03 +0800

    [UPDATED] Thu Aug  8 22:35:02 CST 2019 :bulb:

--- p56-knapsack-problem.cpp
+++ p56-knapsack-problem.cpp
@@ -109,6 +109,30 @@ void solveMulti2() {
   }
 }
 
+int deq[MAX_N + 1];
+int deqv[MAX_N + 1];
+
+void solveMulti3Deque() {
+  for(int i = 0; i < N; i++) {
+    for(int b = 0; b < w[i]; b++) {
+      int s = 0, t = 0;
+      for(int k = 0; k * w[i] + b <= W; k++) {
+        int val = dp[N][k * w[i] + b] - k * v[i];
+        while(s < t && deqv[t - 1] <= val)
+          t--;
+        deq[t] = k;
+        deqv[t++] = val;
+
+        int myMax = deqv[s]; // pop from front of deque
+        dp[N][k * w[i] + b] = myMax + k * v[i];
+        
+        if(deq[s] == k - m[i])
+          s++;
+      }
+    }
+  }
+}
+
 int main() {
   //N = 4; W = 5;
   //int w1[] = {2, 1, 3, 2};
@@ -122,24 +146,25 @@ int main() {
   //memcpy(v, v2, sizeof(v2));
   //solve01fixW();
   
-  N = 3; W = 7;
-  int w3[] = {3, 4, 2};
-  int v3[] = {4, 5, 3};
-  memcpy(w, w3, sizeof(w3));
-  memcpy(v, v3, sizeof(v3));
+  //N = 3; W = 7;
+  //int w3[] = {3, 4, 2};
+  //int v3[] = {4, 5, 3};
+  //memcpy(w, w3, sizeof(w3));
+  //memcpy(v, v3, sizeof(v3));
   //solveFull1();
-  solveFull2();
+  //solveFull2();
   //solveFull3();
  
-  //N = 3; W = 12;
-  //int w4[] = {3, 2, 4};
-  //int v4[] = {2, 4, 3};
-  //int m4[] = {5, 4, 3};
-  //memcpy(w, w4, sizeof(w4));
-  //memcpy(v, v4, sizeof(v4));
-  //memcpy(m, m4, sizeof(m4));
+  N = 3; W = 12;
+  int w4[] = {3, 2, 4};
+  int v4[] = {2, 4, 3};
+  int m4[] = {5, 1, 3};
+  memcpy(w, w4, sizeof(w4));
+  memcpy(v, v4, sizeof(v4));
+  memcpy(m, m4, sizeof(m4));
   //solveMulti1();
   //solveMulti2();
+  solveMulti3Deque();
 
   printf("answer: %d\n", dp[N][W]);
   return 0;
