56ae2fa6844c4962fbf1d3f016d1544997cad2a2
AdrianHsu <adrianhsu1995@gmail.com>
2019-08-15 23:30:00 +0800

    [UPDATED] Thu Aug 15 23:30:00 CST 2019 :bulb:

--- p366-iddfs-power-calculus.cpp
+++ p366-iddfs-power-calculus.cpp
@@ -1,32 +1,39 @@
 #include <cstdio>
+#include <iostream>
 #include <cstring>
 using namespace std;
 
 const int MAX_N = 1010;
-int num[MAX_N], N, maxH;
+int num[MAX_N], N, depth;
 
-bool iddfs(int n, int curH)
-{ 
-  if(n << (maxH - curH) < N) return 0;
-  if(curH > maxH) return 0;
+bool iddfs(int n, int h) { 
+  if(n << (depth - h) < N) return 0;
+  if(h > depth) return 0;
   if(n == N) return 1;
-  num[curH] = n;
-  for(int i = 0; i <= curH; i++){
-    if(iddfs(n + num[i], curH + 1)) return 1;
-    if(iddfs(n - num[i], curH + 1)) return 1; 
+  
+  for(int i = 0; i < h; i++)
+    cout << "....";
+  cout << n << endl;
+  
+  num[h] = n;
+  for(int i = 0; i <= h; i++){
+    if(iddfs(n + num[i], h + 1)) return 1;
+    if(iddfs(n - num[i], h + 1)) return 1; 
   }
   return 0;
 }
 void solve() {
   while(!iddfs(1, 0)){
+    cout << "depth: " << depth << endl;
     memset(num, 0, sizeof(num));
-    maxH++;
+    depth++;
   }
-  printf("%d\n",maxH);
+  printf("ans: %d\n",depth);
 }
 int main() {
   while(scanf("%d",&N) && N){
-    maxH = 0, memset(num, 0, sizeof(num));
+    depth = 0;
+    memset(num, 0, sizeof(num));
     solve();
   }
   return 0;
