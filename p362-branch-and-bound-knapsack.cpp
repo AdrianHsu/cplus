6eeb9a706c68be381df758c386289f9acbe00550
AdrianHsu <adrianhsu1995@gmail.com>
2019-08-18 13:18:01 +0800

    [UPDATED] Sun Aug 18 13:18:01 CST 2019 :bulb:

---
+++ p362-branch-and-bound-knapsack.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <iostream>
+
+#define MAX_N 15
+
+using namespace std;
+
+int N;
+double W;
+double w[MAX_N + 1];
+int c[MAX_N + 1];
+
+void solve() {
+
+}
+
+int main() {
+  N = 5;
+  W = 10.0;
+  int c1 [] = {40, 50, 100, 95, 30};
+  double w1[] = {2, 3.14, 1.98, 5, 3};
+
+  solve();
+  return 0;
+}
