3ae80aea0d625b3603582c354c150ccf12842d18
AdrianHsu <adrianhsu1995@gmail.com>
2019-08-23 17:57:16 +0800

    [UPDATED] Fri Aug 23 17:57:16 CST 2019 :bulb:

--- p384-ac-automation.cpp
+++ p384-ac-automation.cpp
@@ -6,8 +6,7 @@
 using namespace std;
 struct Node {
   Node* child[MAX_N];
-  int count; // 一個標誌，用來標記該結點處是否構成一個單詞（關鍵字）
-  // count > 0 代表他確實是一個關鍵字
+  int cnt;
 };
 
 Node* root;
@@ -17,42 +16,37 @@ void insert(string str) {
     char c = str[i];
     if(now->child[c - 'a'] == NULL) {
       Node* _next = new Node();
-      _next->count = 0;
+      _next->cnt = 0;
       memset(_next->child, 0, sizeof(_next->child));
       now->child[c - 'a'] = _next;
     }
     now = now->child[c - 'a'];
   }
-  now->count++;
+  now->cnt++;
 }
-int search(string str) {
+bool search(string str) {
   Node *now = root;
-  int cnt = 0;
   for(int i = 0; i < str.length(); i++) {
     char c = str[i];
     if(now->child[c - 'a'] != NULL) {
       now = now->child[c - 'a'];
     } else {
-      return 0;
+      return false;
     }
   }
-  return now->count;
+  return now->cnt;
 }
 void solve() {
   root = new Node();
-  root->count = 0;
   memset(root->child, 0, sizeof(root->child));
   
   insert("abcde");
   insert("acde");
-  insert("abce");
   insert("bce");
-  insert("bce");
-  insert("ce");
   cout << search("bce") << endl;
   cout << search("abce") << endl;
   cout << search("abcde") << endl;
-  cout << search("abcdefbce") << endl;
+  cout << search("abcdef") << endl;
 }
 int main() {
   
