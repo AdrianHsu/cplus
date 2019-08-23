dfa294d9946a385e3c58a05c8f4dab921b138858
AdrianHsu <adrianhsu1995@gmail.com>
2019-08-23 18:21:40 +0800

    [UPDATED] Fri Aug 23 18:21:40 CST 2019 :bulb:

--- p384-ac-automation.cpp
+++ p384-ac-automation.cpp
@@ -14,7 +14,7 @@ struct Node {
   Node(){
     fail = NULL;
     cnt = 0;
-    memset(child, NULL, sizeof(child));
+    memset(child, 0, sizeof(child));
   }
 };
 
@@ -32,20 +32,79 @@ void insert(string str) {
   now->cnt++;
 }
 void acAutomation() {
+
+  queue<Node*> Q;
+  Node *now;
+  Node *f;
+  Q.push(root);
+
+  while(!Q.empty()) {
+    Node* now = Q.front();
+    Q.pop();
+    for(int i = 0; i < MAX_N; i++) {
+      if(now->child[i] == NULL) continue;
+
+      if(now == root)
+        now->child[i]->fail = root;
+      else {
+        f = now->fail;
+        while(f != NULL) {
+          if(f->child[i] != NULL) {
+            now->child[i]->fail = f->child[i];
+            break;
+          }
+          f = f->fail;
+        }
+        if(f == NULL)
+          now->child[i]->fail = root;
+      }
+      Q.push(now->child[i]);
+    }
+  }
+}
+int query(string s) {
+  int index, cnt = 0;
+  Node *now = root;
+  Node *tmp;
+
+  for(int i = 0; i < s.length(); i++) {
+    index = s[i] - 'a';
+    while(now->child[index] == NULL && now->fail != NULL)
+      now = now->fail;
+    if(now->child[index] != NULL) {
+      now = now->child[index];
+      tmp = now;
+      while(tmp->cnt > 0) {
+        cnt += tmp->cnt;
+        tmp->cnt = -1; // after this time, it will not be added again becuz -1 < 0
+        tmp = tmp->fail;
+      }
+    }
+  }
+  return cnt;
 }
 
 void solve() {
   root = new Node();
-  memset(root->child, 0, sizeof(root->child));
   
-  insert("say");
-  insert("she");
-  insert("shr");
-  insert("he");
-  insert("her");
-  
-  acAutomation(); // after this, you cannot insert later on
+ // insert("say");
+ // insert("she");
+ // insert("shr");
+ // insert("he");
+ // insert("her");
+ 
+  insert("a");
+  insert("ab");
+  insert("bab");
+  insert("bc");
+  insert("bca");
+  insert("c");
+  insert("caa");
 
+  acAutomation(); // after this, you cannot insert later on
+  //string target = "yasherhs";
+  string target = "abccab";
+  cout << query(target) << endl;
 }
 int main() {
   
