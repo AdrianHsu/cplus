5aef8ad7308004abf86e122957713c37e7f56fb0
AdrianHsu <adrianhsu1995@gmail.com>
2019-08-27 00:35:59 +0800

    [UPDATED] Tue Aug 27 00:35:59 CST 2019 :bulb:

--- p384-ac-automation-array.cpp
+++ p384-ac-automation-array.cpp
@@ -16,7 +16,7 @@ using namespace std;
 typedef long long ll;
 
 struct acAutomata {
-  int ch[MAXS][MAXC]; //child
+  int go[MAXS][MAXC]; //child
   int f[MAXS];
   int out[MAXS];
   int pos;
@@ -25,7 +25,7 @@ struct acAutomata {
     
     pos = 1; // Initially, we just have the 0 state 
     memset(out, 0, sizeof(out));
-    memset(ch, -1, sizeof(ch));
+    memset(go, -1, sizeof(go));
     memset(f, -1, sizeof(f));
   }
   int idx(char c) {
@@ -33,27 +33,27 @@ struct acAutomata {
   }
   
   void insert(string s, int i) {
-    int dep = 0;
+    int state = 0;
     for(int i = 0; i < s.length(); i++) {
-      int c = idx(s[i]);
-      if(ch[dep][c] == -1) 
-        ch[dep][c] = pos++;
+      int ch = idx(s[i]);
+      if(go[state][ch] == -1) 
+        go[state][ch] = pos++;
       
-      dep = ch[dep][c];
+      state = go[state][ch];
     }
-    out[dep] |= (1 << i);
+    out[state] |= (1 << i);
   }
   
   void build() {
 
-    for(int i = 0; i < MAXC; i++)
-      if(ch[0][i] == -1)
-        ch[0][i] = 0;
+    for(int i = 0; i < MAXC; i++) 
+      if(go[0][i] == -1)
+        go[0][i] = 0;
 
     // for depth = 1
     queue<int> Q;
     for(int i = 0; i < MAXC; i++) {
-      int child = ch[0][i];
+      int child = go[0][i];
       if(child > 0) { 
         f[child] = 0;
         Q.push(child);
@@ -64,14 +64,15 @@ struct acAutomata {
     while(!Q.empty()) {
       int r = Q.front(); Q.pop();
       for(int i = 0; i < MAXC; i++) { 
-        int child = ch[r][i];
+        int child = go[r][i];
         if(child == -1)
           continue;
+
         int fail = f[r];
-        while(ch[fail][i] == -1) {
+        while(go[fail][i] == -1) {
           fail = f[ fail ];
         }
-        fail = ch[fail][i];
+        fail = go[fail][i];
         f[child] = fail;
 
         out[child] |= out[fail];
@@ -81,39 +82,36 @@ struct acAutomata {
   }
 
   int find(int state, char c) {
-    int ans = state;
-    int i = idx(c);
-    while(ch[ans][i] == -1)
-      ans = f[ans];
-
-    return ch[ans][i];
+    while(go[state][idx(c)] == -1)
+      state = f[state];
+    return go[state][idx(c)];
   }
 
 } ac;
 
 void solve() {
   ac.clear();
-  int arr_size = 5;
+  int M = 5;
   string arr[] = {"say", "she", "shr", "he", "her"};
   string target = "yasherhs";
   // string arr[] = {"a", "ab", "bab", "bc", "bca", "c", "caa"};
   // string target = "abccab";
 
-  for(int i = 0; i < arr_size; i++) {
+  for(int i = 0; i < M; i++) {
     ac.insert(arr[i], i);
   }
   ac.build();
-  // cout << ac.f[0] << endl;
-  // cout << ac.f[1] << endl;
 
   int state = 0;
   int cnt = 0;
   for(int i = 0; i < target.length(); i++) {
-    state = ac.find(state, target[i]);
+    char ch = target[i];
+    state = ac.find(state, ch);
 
     if(ac.out[state] == 0)
       continue;
-    for(int j = 0; j < arr_size; j++) {
+
+    for(int j = 0; j < M; j++) {
       if(ac.out[state] & (1 << j))
         cnt++;
       // if(ac.out[state] & (1 << j))
